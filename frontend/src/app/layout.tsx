5e15c4e9881292693c1cc7167aca2a550052216a
David Batista <xmedavid@users.noreply.github.com>
2024-05-22 14:15:58 +0100

    fix: links to homepage based on auth

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "~/styles/globals.css";
 import { Inter as InterSans } from "next/font/google";
 import { cn } from "~/lib/utils";
 
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { CodeIcon } from "~/components/icons";
 
@@ -36,10 +36,18 @@ export default function RootLayout({
       >
         <body className="pt-16">
           <section className="fixed top-0 w-full h-16 flex items-center justify-between bg-gray-900 px-6 py-4 text-white shadow-lg">
-            <Link className="flex items-center gap-2" href="#">
-              <CodeIcon className="h-6 w-6" />
-              <span className="text-lg font-semibold">Wolkenlauf</span>
-            </Link>
+            <SignedIn>
+              <Link className="flex items-center gap-2" href="/dashboard">
+                <CodeIcon className="h-6 w-6" />
+                <span className="text-lg font-semibold">Wolkenlauf</span>
+              </Link>
+            </SignedIn>
+            <SignedOut>
+              <Link className="flex items-center gap-2" href="/">
+                <CodeIcon className="h-6 w-6" />
+                <span className="text-lg font-semibold">Wolkenlauf</span>
+              </Link>
+            </SignedOut>
             <SignedIn>
               <UserButton
                 appearance={{
